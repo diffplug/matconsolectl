2da821acc83b5403228d694ed66cb086a3a35c92
nonother <nonother@b33616ba-1504-11df-94bc-6d6fff283d55>
2011-06-24 09:49:09 +0000

    Additional javadoc comments.

--- matlabcontrol/src/matlabcontrol/package-info.java
+++ matlabcontrol/src/matlabcontrol/package-info.java
@@ -1,7 +1,17 @@
 /**
- * Contains the core functionality to interact with MATLAB. To interact with MATLAB start by creating a
- * {@link matlabcontrol.MatlabProxyFactory}, and then use it to create a {@link matlabcontrol.MatlabProxy}. All classes
- * in this package are unconditionally thread-safe.
+ * Contains the core functionality to interact with MATLAB. All classes in this package are unconditionally thread-safe.
+ * To interact with MATLAB start by creating a {@link matlabcontrol.MatlabProxyFactory}, and then use it to create a
+ * {@link matlabcontrol.MatlabProxy}. The most basic usage pattern of matlabcontrol:
+ * <pre>
+ * {@code
+ * MatlabProxyFactory factory = new MatlabProxyFactory();
+ * MatlabProxy proxy = factory.getProxy();
+ * ...
+ * //Use the proxy as desired 
+ * ...
+ * proxy.disconnect();
+ * }
+ * </pre>
  * 
  * @since 3.0.0
  */
