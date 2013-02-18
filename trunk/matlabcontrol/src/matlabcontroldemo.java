e1b3bd25e2ae91b683746d42e1112b7447d35030
nonother@gmail.com <nonother@gmail.com@b33616ba-1504-11df-94bc-6d6fff283d55>
2013-02-18 07:13:46 +0000

    Updates the license copyright info year to 2013.

--- trunk/matlabcontrol/src/matlabcontroldemo.java
+++ trunk/matlabcontrol/src/matlabcontroldemo.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2011, Joshua Kaplan
+ * Copyright (c) 2013, Joshua Kaplan
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
