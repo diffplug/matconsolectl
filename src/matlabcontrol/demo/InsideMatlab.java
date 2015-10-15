a2ba0c4d0d39e3ea0c5c057859516979eef3f688
Ned Twigg <ned.twigg@diffplug.com>
2015-10-15 15:57:45 -0700

    Collapsed the project and its demo into a single project.

--- src/matlabcontrol/demo/InsideMatlab.java
+++ src/matlabcontrol/demo/InsideMatlab.java
@@ -1,3 +1,8 @@
+/*
+ * Code licensed under new-style BSD (see LICENSE).
+ * All code up to tags/original: Copyright (c) 2013, Joshua Kaplan
+ * All code after tags/original: Copyright (c) 2015, DiffPlug
+ */
 package matlabcontrol.demo;
 
 import java.awt.EventQueue;
---
+++ src/matlabcontrol/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Code licensed under new-style BSD (see LICENSE).
+ * All code up to tags/original: Copyright (c) 2013, Joshua Kaplan
+ * All code after tags/original: Copyright (c) 2015, DiffPlug
+ */
+package matlabcontrol;
