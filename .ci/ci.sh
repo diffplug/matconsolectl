a14fdef89140102bac777c42dfed54bee9bc561d
Ned Twigg <ned.twigg@diffplug.com>
2015-10-15 16:18:46 -0700

    Fixed CI scripts.

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -3,7 +3,7 @@
 # Do the Gradle build
 ./gradlew build || exit 1
 
-if [ "$TRAVIS_REPO_SLUG" == "diffplug/matlabcontrol" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "diffplug/matconsolectl" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 	# Publish the artifacts
 	./gradlew publish || exit 1
 	# Push the javadoc
