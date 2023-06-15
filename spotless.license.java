438160808e7574c37bb82664cc3435a5c883259f
Ned Twigg <ned.twigg@diffplug.com>
2023-06-15 10:27:32 -0700

    Set to our project standard.

--- spotless.license.java
+++
@@ -1,5 +0,0 @@
-/*
- * Code licensed under new-style BSD (see LICENSE).
- * All code up to tags/original: Copyright (c) 2013, Joshua Kaplan
- * All code after tags/original: Copyright (c) 2016, DiffPlug
- */
